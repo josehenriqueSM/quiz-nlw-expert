2ce9ea7b79e1ad5f3fa4fb0e1116547472c01e2a
josehenriqueSM <jhenriquemacedo99@gmail.com>
2024-02-11 10:27:32 -0300

    changed variables to English

--- index.js
+++ index.js
@@ -1,122 +1,122 @@
-const perguntas = [
+const questions = [
     {
-      pergunta: "Qual palavra-chave é usada para declarar uma variável em JavaScript?",
-      resposta: [
+      question: "Qual palavra-chave é usada para declarar uma variável em JavaScript?",
+      answer: [
         "var",
         "let",
         "const",
       ],
-      correta: 2
+      correct: 2
     },
     {
-      pergunta: "Qual dos seguintes métodos é usado para adicionar um elemento ao final de um array?",
-      resposta: [
+      question: "Qual dos seguintes métodos é usado para adicionar um elemento ao final de um array?",
+      answer: [
         "append()",
         "push()",
         "addToEnd()",
       ],
-      correta: 1
+      correct: 1
     },
     {
-      pergunta: "O que o operador '===' faz em JavaScript?",
-      resposta: [
+      question: "O que o operador '===' faz em JavaScript?",
+      answer: [
         "Compara valores e tipos sem coerção",
         "Compara valores com coerção",
         "Atribui valores",
       ],
-      correta: 0
+      correct: 0
     },
     {
-      pergunta: "Como você escreve um comentário de linha em JavaScript?",
-      resposta: [
+      question: "Como você escreve um comentário de linha em JavaScript?",
+      answer: [
         "// Comentário",
         "/* Comentário */",
         "# Comentário",
       ],
-      correta: 0
+      correct: 0
     },
     {
-      pergunta: "Qual função é usada para converter uma string em um número inteiro em JavaScript?",
-      resposta: [
+      question: "Qual função é usada para converter uma string em um número inteiro em JavaScript?",
+      answer: [
         "parseInt()",
         "parseFloat()",
         "toInteger()",
       ],
-      correta: 0
+      correct: 0
     },
     {
-      pergunta: "O que o método 'map()' faz em um array?",
-      resposta: [
+      question: "O que o método 'map()' faz em um array?",
+      answer: [
         "Filtra os elementos do array",
         "Mapeia os elementos do array para uma nova array",
         "Ordena os elementos do array",
       ],
-      correta: 1
+      correct: 1
     },
     {
-      pergunta: "Qual é a função do operador '&&' em JavaScript?",
-      resposta: [
+      question: "Qual é a função do operador '&&' em JavaScript?",
+      answer: [
         "Operador de adição",
         "Operador de concatenação",
         "Operador lógico AND",
       ],
-      correta: 2
+      correct: 2
     },
     {
-      pergunta: "O que a declaração 'let' faz em JavaScript?",
-      resposta: [
+      question: "O que a declaração 'let' faz em JavaScript?",
+      answer: [
         "Declara uma constante",
         "Declara uma variável com escopo de bloco",
         "Declara uma função",
       ],
-      correta: 1
+      correct: 1
     },
     {
-      pergunta: "Qual método é usado para remover o último elemento de um array em JavaScript?",
-      resposta: [
+      question: "Qual método é usado para remover o último elemento de um array em JavaScript?",
+      answer: [
         "pop()",
         "remove()",
         "deleteLast()",
       ],
-      correta: 0
+      correct: 0
     },
     {
-      pergunta: "O que é o conceito de 'hoisting' em JavaScript?",
-      resposta: [
+      question: "O que é o conceito de 'hoisting' em JavaScript?",
+      answer: [
         "Elevação de objetos",
         "Elevação de funções",
         "Elevação de variáveis",
       ],
-      correta: 2
+      correct: 2
     }
   ];
   
   const quiz = document.querySelector('#quiz');
   const template = document.querySelector('template');
   
-  const corretas = new Set();
-  const totalDePerguntas = perguntas.length;
-  const mostrarTotal = document.querySelector('#acertos span');
-  mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
+  const corrects = new Set();
+  const totalOfQuestions = questions.length;
+  const showTotal = document.querySelector('#acertos span');
+  showTotal.textContent = corrects.size + ' de ' + totalOfQuestions;
   
   //loop ou laço de repetição
-  for(const item of perguntas) {
+  for(const item of questions) {
     const quizItem = template.content.cloneNode(true);
-    quizItem.querySelector('h3').textContent = item.pergunta;
+    quizItem.querySelector('h3').textContent = item.question;
     
-    for(let resposta of item.resposta) {
+    for(let answer of item.answer) {
       const dt = quizItem.querySelector('dl dt').cloneNode(true);
-      dt.querySelector('span').textContent = resposta;
-      dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item));
-      dt.querySelector('input').value = item.resposta.indexOf(resposta);
+      dt.querySelector('span').textContent = answer;
+      dt.querySelector('input').setAttribute('name', 'question-' + questions.indexOf(item));
+      dt.querySelector('input').value = item.answer.indexOf(answer);
       dt.querySelector('input').onchange = (event) => {
-        const estaCorreta = event.target.value == item.correta;
+        const thisCorrect = event.target.value == item.correct;
         
-        corretas.delete(item);
-        if(estaCorreta) {
-          corretas.add(item);
+        corrects.delete(item);
+        if(thisCorrect) {
+          corrects.add(item);
         }
-       mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
+       showTotal.textContent = corrects.size + ' de ' + totalOfQuestions;
   
       }
       
@@ -124,6 +124,6 @@ const perguntas = [
     }
     quizItem.querySelector('dl dt').remove();
     
-    // coloca as pergunta na tela
+    // coloca as question na tela
     quiz.appendChild(quizItem);
   }
